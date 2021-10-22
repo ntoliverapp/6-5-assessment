c3735a227d69a18340a630e0b1f146168604bcdf
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-22 10:10:09 -0700

    passed center grid X test

---
+++ tictactoe.test.ts
@@ -0,0 +1,28 @@
+import { hasUncaughtExceptionCaptureCallback } from "process"
+import { Builder, Capabilities, By } from "selenium-webdriver"
+
+const chromedriver = require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeAll(async () => {
+    await driver.get('http://localhost:4000')
+})
+
+afterAll(async () => {
+    await driver.quit()
+})
+
+test('I can start a game', async () => {
+
+    let button = await (await driver).findElement(By.id('start-game'));
+    await button.click();
+    
+});
+
+test('X should appear on center grid', async () => {
+    const centerClick = await (await driver).findElement(By.id("cell-4"));
+    console.log(centerClick)
+    await centerClick.click();
+
+});
